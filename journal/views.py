9c25741395d9b1f0d76c10b5a2363ec0ecc876ab
justinmerville <justinmerville1@gmail.com>
2021-12-03 03:05:19 -0500

    Added base html & extended it to the htmls created. Created a register html and login html

--- journal/views.py
+++ journal/views.py
@@ -1,4 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .forms import CreateUserForm
+
 
 def index(request):
     return render(request, 'entry/index.html')
+
+
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account was created!')
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'User Authentication/register.html', context)
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+
+    context = {}
+    return render(request, 'User Authentication/login.html', context)
